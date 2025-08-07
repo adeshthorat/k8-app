261f7e895c4f5111cf4c073fc38f6d60b1d7a047
adeshthorat <aadeshthorat332000@gmail.com>
2025-08-07 17:11:45 +0530

    added src code

---
+++ k8app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Welcome to Python API v1", "status": "running"})
+
+@app.route('/hello/<name>', methods=['GET'])
+def hello(name):
+    return jsonify({"message": f"Hello, {name}!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
